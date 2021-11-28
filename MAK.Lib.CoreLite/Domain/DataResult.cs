de203d212d8173222eaa667005e16f98793c9375
Maurice Kelly <kay86@live.com>
2021-11-28 16:30:23 +0000

    Commit from VS

---
+++ MAK.Lib.CoreLite/Domain/DataResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+
+public class DataResult<TData>
+{
+    public TData? Data { get; set; }
+    public bool Success { get; set; } = true;
+    public string? Message { get; set; }
+}
