dfff094e1288f9cf88fcc4ffa7e2f9f4ce8933dd
Maurice Kelly <kay86@live.com>
2022-03-19 11:24:42 +0000

    Upgate project structure.

--- Domain/DataResult.cs
+++ Domain/DataResult.cs
@@ -2,7 +2,7 @@
 
 public class DataResult<TData>
 {
-    public TData? Data { get; set; }
+    public TData Data { get; set; }
     public bool Success { get; set; } = true;
-    public string? Message { get; set; }
+    public string Message { get; set; }
 }
---
+++ docs/README.md
@@ -0,0 +1,4 @@
+# Solution: CoreLite
+### Programmer: Maurice Antonio Kelly @MAKMaurice
+### NuGet: MAK.Lib.CoreLite
+### CoreLite is a .NET library that has core types that can be used by various types of .NET projects.
